f7449c1eb6610f6074f7f162698ae9c67d5155a6
SharpAceX <sharpacex@gmail.com>
2017-03-04 14:55:44 -0500

    Added contact to data

--- javascript/data.js
+++ javascript/data.js
@@ -1,48 +1,66 @@
 var data = {
-  projects: {
-    "trumpulse": {
-      name: "Trumpulse",
-      background: "#2f61de",
-      color: "#ffffff",
-      date: "March 2017",
-      description: "Trumpulse uses data from multiple news sources so you can get the pulse of America under Trump. It is a single-page application made using Angular, HTML, CSS, and JavaScript. Using data from various news sources and social networks like Facebook and Twitter, you can get a diverse picture of the state of America under the Trump administration.",
-      images: ["image1", "image2", "image3"],
-      technologies: [
-        "Cordova",
-        "Angular",
-        "HTML 5",
-        "CSS 3",
-        "JavaScript"
-      ],
-      platforms: [
-        "Web",
-        "Android"
-      ],
-      links: {
-        "Demo": "http://alanmorel.com/trump",
-      },
+    contact: {
+        github: {
+            name: "Github",
+            link: "https://github.com/AlanMorel"
+        },
+        linkedin: {
+            name: "LinkedIn",
+            link: "https://linkedin.com/in/AlanMorel/",
+        },
+        facebook: {
+            name: "Facebook",
+            link: "https://www.facebook.com/SharpAceX"
+        },
+        twitter: {
+            name: "Twitter",
+            link: "https://www.twitter.com/SharpAceX"
+        }
     },
-    "chainball": {
-      name: "Chainball",
-      background: "#f63f6a",
-      color: "#ffffff",
-      date: "July 2014",
-      description: "Use your fast reflexives to build yourself a chain as you navigate the endless stream of circles. Hit the blue and green circles while avoiding the red and gray ones to score as high as you can. When you’re finished, brag to your friends and challenge them to score higher than you in Chainball!",
-      images: ["image1", "image2", "image3", "image4"],
-      credits: {
-        "Framework": "LibGDX",
-        "Music": "NoSoapRadio.us"
-      },
-      technologies:[
-        "Java",
-        "libGDX"
-      ],
-      platforms: [
-        "Android"
-      ],
-      links: {
-        "Google Play Store": "https://play.google.com/store/apps/details?id=com.luminositygames.chainball.android",
-      },
+    projects: {
+        "trumpulse": {
+            name: "Trumpulse",
+            background: "#2f61de",
+            color: "#ffffff",
+            date: "March 2017",
+            description: "Trumpulse uses data from multiple news sources so you can get the pulse of America under Trump. It is a single-page application made using Angular, HTML, CSS, and JavaScript. Using data from various news sources and social networks like Facebook and Twitter, you can get a diverse picture of the state of America under the Trump administration.",
+            images: ["image1", "image2", "image3"],
+            technologies: [
+                "Cordova",
+                "Angular",
+                "HTML 5",
+                "CSS 3",
+                "JavaScript"
+            ],
+            platforms: [
+                "Web",
+                "Android"
+            ],
+            links: {
+                "Demo": "http://alanmorel.com/trump",
+            },
+        },
+        "chainball": {
+            name: "Chainball",
+            background: "#f63f6a",
+            color: "#ffffff",
+            date: "July 2014",
+            description: "Use your fast reflexives to build yourself a chain as you navigate the endless stream of circles. Hit the blue and green circles while avoiding the red and gray ones to score as high as you can. When you’re finished, brag to your friends and challenge them to score higher than you in Chainball!",
+            images: ["image1", "image2", "image3", "image4"],
+            credits: {
+                "Framework": "LibGDX",
+                "Music": "NoSoapRadio.us"
+            },
+            technologies: [
+                "Java",
+                "libGDX"
+            ],
+            platforms: [
+                "Android"
+            ],
+            links: {
+                "Google Play Store": "https://play.google.com/store/apps/details?id=com.luminositygames.chainball.android",
+            },
+        }
     }
-  }
 };
