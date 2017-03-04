967c20f03ad50e2528193dc203c6bdfa3333fa4a
SharpAceX <sharpacex@gmail.com>
2017-03-04 18:35:05 -0500

    Data.js clean up

--- javascript/data.js
+++ javascript/data.js
@@ -1,21 +1,9 @@
 var data = {
     contacts: {
-        github: {
-            name: "Github",
-            link: "https://github.com/AlanMorel"
-        },
-        linkedin: {
-            name: "LinkedIn",
-            link: "https://linkedin.com/in/AlanMorel/",
-        },
-        facebook: {
-            name: "Facebook",
-            link: "https://www.facebook.com/SharpAceX"
-        },
-        twitter: {
-            name: "Twitter",
-            link: "https://www.twitter.com/SharpAceX"
-        }
+        "Github": "https://github.com/AlanMorel",
+        "LinkedIn": "https://linkedin.com/in/AlanMorel/",
+        "Facebook": "https://www.facebook.com/SharpAceX",
+        "Twitter": "https://www.twitter.com/SharpAceX"
     },
     projects: {
         "trumpulse": {
@@ -26,17 +14,8 @@ var data = {
             date: "March 2017",
             description: "Trumpulse uses data from multiple news sources so you can get the pulse of America under Trump. It is a single-page application made using Angular, HTML, CSS, and JavaScript. Using data from various news sources and social networks like Facebook and Twitter, you can get a diverse picture of the state of America under the Trump administration.",
             images: ["image1", "image2", "image3"],
-            technologies: [
-                "Cordova",
-                "Angular",
-                "HTML 5",
-                "CSS 3",
-                "JavaScript"
-            ],
-            platforms: [
-                "Web",
-                "Android"
-            ],
+            technologies: ["Cordova", "Angular", "HTML 5", "CSS 3", "JavaScript"],
+            platforms: ["Web", "Android"],
             links: {
                 "Demo": "http://alanmorel.com/trump",
             },
@@ -49,17 +28,12 @@ var data = {
             date: "July 2014",
             description: "Use your fast reflexives to build yourself a chain as you navigate the endless stream of circles. Hit the blue and green circles while avoiding the red and gray ones to score as high as you can. When you’re finished, brag to your friends and challenge them to score higher than you in Chainball!",
             images: ["image1", "image2", "image3", "image4"],
+            technologies: ["Java", "libGDX" ],
+            platforms: ["Android"],
             credits: {
                 "Framework": "LibGDX",
                 "Music": "NoSoapRadio.us"
             },
-            technologies: [
-                "Java",
-                "libGDX"
-            ],
-            platforms: [
-                "Android"
-            ],
             links: {
                 "Google Play Store": "https://play.google.com/store/apps/details?id=com.luminositygames.chainball.android",
             },
@@ -72,12 +46,8 @@ var data = {
             date: "August 2011",
             description: "Start off with one balloon and soon end up with many! Every 5 levels unlocks yet another balloon so things will become interesting real quick! Your goal is to obtain as high of a score as possible without missing too many balloons.",
             images: ["image1", "image2", "image3", "image4", "image5"],
-            technologies: [
-                "Java",
-            ],
-            platforms: [
-                "Android"
-            ],
+            technologies: ["Java"],
+            platforms: ["Android"],
             links: {
                 "Google Play Store": "https://play.google.com/store/apps/details?id=com.sharpacex.BalloonFrenzy",
             },
@@ -90,13 +60,8 @@ var data = {
             date: "June 2012",
             description: "Your job is simple. Just survive. With just your jumping and ducking skills at your disposal, you will need to avoid incoming missiles to keep yourself alive. The longer you can stay alive, the more score you’ll earn! At the end of the game your performance will be ranked from an (A+) to and (F-)! Be warned, this can get addicting!",
             images: ["image1", "image2", "image3", "image4", "image5", "image6", "image7"],
-            technologies: [
-                "Java",
-            ],
-            platforms: [
-                "Android",
-                "Windows"
-            ],
+            technologies: ["Java"],
+            platforms: ["Android", "Windows"],
             links: {
                 "Google Play Store": "https://play.google.com/store/apps/details?id=com.sharpacex.dangerzone",
             },
