d90a01c127d3d9bf889dd8c1547f8f538844c1ab
SharpAceX <sharpacex@gmail.com>
2017-03-14 02:30:25 -0400

    Added dynamic action message

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,18 @@
 render("contacts");
 render("projects");
 
-function render(template){
-  var source = document.getElementById(template + "-template").innerHTML;
-  var destination = document.querySelector("." + template);
-  destination.innerHTML = Handlebars.compile(source)(data[template]);
+function render(template) {
+    var source = document.getElementById(template + "-template").innerHTML;
+    var destination = document.querySelector("." + template);
+    destination.innerHTML = Handlebars.compile(source)(data[template]);
 }
+
+var action = document.getElementById("action");
+var actions = ["collaborate", "create", "share", "build", "learn", "code", "teach", "dream"];
+
+var i = 0;
+
+setInterval(() => {
+    action.innerHTML = actions[i];
+    i = (i + 1) % actions.length;
+}, 1500);
