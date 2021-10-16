4bf11b49a88ff57c9026293879d12e613e965d20
Alan Morel <alan@alanmorel.com>
2021-10-16 14:37:50 -0400

    Remove redirecting log

--- server/controllers/get/LinkController.ts
+++ server/controllers/get/LinkController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
-export default function(link: string): (req: Request, res: Response) => void {
+export default function (link: string): (req: Request, res: Response) => void {
     return (req: Request, res: Response): void => res.redirect(link);
 }
--- server/controllers/get/RewriteController.ts
+++ server/controllers/get/RewriteController.ts
@@ -1,9 +1,7 @@
-import { Logger } from "@/tools/Logger";
 import { NextFunction, Request, Response } from "express";
 
 export default function (path: string): (req: Request, res: Response, next: NextFunction) => void {
     return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        Logger.log("Redirecting " + req.url + " to " + path);
         req.url = path;
         next();
     };
