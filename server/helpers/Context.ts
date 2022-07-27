f5f598447c2845352e443ac9f100327b6c2266c0
Alan Morel <alan@alanmorel.com>
2022-07-27 10:40:22 -0400

    Update Context

--- server/helpers/Context.ts
+++ server/helpers/Context.ts
@@ -1,15 +1,27 @@
 import Config from "@/server/Config";
 import { Request } from "express";
 
+const app = {
+    slug: "alanmorel",
+    domain: "alanmorel.com",
+    partials: `${Config.root}/server/views/partials`
+};
+
+const configs = {
+    env: Config.env,
+    root: Config.root,
+    version: Config.version
+};
+
 export default function (req: Request): any {
-    return {
-        slug: "alanmorel",
-        domain: "alanmorel.com",
-        env: Config.env,
-        now: new Date(),
+    const framework = {
         base: `${req.protocol}://${req.get("host")}`,
-        canonical: req.originalUrl,
-        root: Config.root,
-        version: Config.version
+        canonical: `${req.protocol}://${req.get("host") + req.path}`
+    };
+
+    return {
+        ...app,
+        ...configs,
+        ...framework
     };
 }
