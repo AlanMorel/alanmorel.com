6c55a857abb053dfa46bcce7ebe87bc9e5e7c751
Alan Morel <alan@alanmorel.com>
2023-03-15 00:21:16 -0400

    fix: icons too wide on safari

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -10,13 +10,7 @@ export default function Contacts(props: Props): JSX.Element {
     const { children, viewBox, fill } = props;
 
     return (
-        <svg
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox={`0 0 ${viewBox}`}
-            fill={fill}
-            className="h-full w-full"
-        >
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${viewBox}`} fill={fill} className="h-full">
             {children}
         </svg>
     );
