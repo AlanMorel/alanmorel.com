4ade7a659ba82575f2cfd0679070485549d5a693
Alan Morel <alan@alanmorel.com>
2023-04-01 11:05:21 -0400

    refactor: require only typescript for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    content: ["./app/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+    content: ["./app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
     theme: {
         extend: {
             fontFamily: {
