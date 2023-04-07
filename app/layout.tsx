901e2b358a771681d9f94477f1e09c92b9969878
Alan Morel <alan@alanmorel.com>
2023-04-07 16:30:22 -0400

    feat: populate the meta data for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import Config from "@/src/Config";
 import "@/src/globals.css";
 import Analytics from "@/src/helpers/client/Analytics";
+import { withMetadata } from "@/src/helpers/server/MetadataHelper";
 import "@fontsource/inter/variable.css";
 import { ReactNode } from "react";
 
@@ -19,98 +19,7 @@ export default async function RootLayout(props: Props): Promise<JSX.Element> {
     );
 }
 
-const title = "Alan Morel";
-const description = "Alan Morel - Software Engineer, Photographer, Music Producer";
-const image = "/images/meta-image.png";
-const favicon = "/favicons/favicon.ico";
-const handle = "@AlanMorelX";
-
-export const metadata = {
-    title: {
-        default: title,
-        template: `%s | ${Config.app.name}`
-    },
-    description: description,
-    applicationName: Config.app.name,
-    colorScheme: "light",
-    openGraph: {
-        title: Config.app.name,
-        description: description,
-        url: `https://${Config.app.domain}`,
-        siteName: Config.app.name,
-        images: [
-            {
-                url: image,
-                width: 1280,
-                height: 800
-            }
-        ],
-        locale: "en-US",
-        type: "website"
-    },
-    icons: {
-        icon: favicon,
-        shortcut: favicon,
-        apple: [
-            {
-                url: "/favicons/apple-touch-icon-57x57.png",
-                sizes: "57x57",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-60x60.png",
-                sizes: "60x60",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-72x72.png",
-                sizes: "72x72",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-76x76.png",
-                sizes: "76x76",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-114x114.png",
-                sizes: "114x114",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-120x120.png",
-                sizes: "120x120",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-144x144.png",
-                sizes: "144x144",
-                type: "image/png"
-            },
-            {
-                url: "/favicons/apple-touch-icon-152x152.png",
-                sizes: "152x152",
-                type: "image/png"
-            }
-        ],
-        other: {
-            rel: "apple-touch-icon-precomposed",
-            url: "/favicons/apple-touch-icon-152x152.png"
-        }
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: title,
-        description: description,
-        creator: handle,
-        images: {
-            url: image,
-            alt: `${Config.app.name} Logo`
-        }
-    },
-    viewport: {
-        width: "device-width",
-        initialScale: 1,
-        minimumScale: 1
-    }
-};
+export const metadata = withMetadata({
+    title: "Alan Morel",
+    description: "Alan Morel - Software Engineer, Photographer, Music Producer"
+});
--- src/helpers/server/MetadataHelper.ts
+++ src/helpers/server/MetadataHelper.ts
@@ -33,18 +33,96 @@ export const withMetadata = (baseMetadata: BaseMetadata): Metadata => {
     return buildMetadata(baseMetadata);
 };
 
+const favicon = "/favicons/favicon.ico";
+const image = "/images/meta-image.png";
+const handle = "@AlanMorelX";
+
 const buildMetadata = (base: BaseMetadata): Metadata => {
     return {
+        metadataBase: new URL(`https://${Config.app.domain}`),
         title: base.title,
         description: base.description,
+        applicationName: Config.app.name,
+        colorScheme: "light",
         openGraph: {
             title: base.title,
             description: base.description,
-            url: `https://${Config.app.domain}`
+            url: `https://${Config.app.domain}`,
+            siteName: Config.app.name,
+            images: [
+                {
+                    url: base.image ?? image,
+                    width: 1280,
+                    height: 800
+                }
+            ],
+            locale: "en-US",
+            type: "website"
         },
         twitter: {
+            card: "summary_large_image",
             title: base.title,
-            description: base.description
+            description: base.description,
+            creator: handle,
+            images: {
+                url: image,
+                alt: `${Config.app.name} Logo`
+            }
+        },
+        icons: {
+            icon: favicon,
+            shortcut: favicon,
+            apple: [
+                {
+                    url: "/favicons/apple-touch-icon-57x57.png",
+                    sizes: "57x57",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-60x60.png",
+                    sizes: "60x60",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-72x72.png",
+                    sizes: "72x72",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-76x76.png",
+                    sizes: "76x76",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-114x114.png",
+                    sizes: "114x114",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-120x120.png",
+                    sizes: "120x120",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-144x144.png",
+                    sizes: "144x144",
+                    type: "image/png"
+                },
+                {
+                    url: "/favicons/apple-touch-icon-152x152.png",
+                    sizes: "152x152",
+                    type: "image/png"
+                }
+            ],
+            other: {
+                rel: "apple-touch-icon-precomposed",
+                url: "/favicons/apple-touch-icon-152x152.png"
+            }
+        },
+        viewport: {
+            width: "device-width",
+            initialScale: 1,
+            minimumScale: 1
         }
     };
 };
