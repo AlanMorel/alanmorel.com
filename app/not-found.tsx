061b11b60aafe502d256fc30e50affb41f9060c5
Alan Morel <alan@alanmorel.com>
2023-04-21 21:00:51 -0400

    feat: add global not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(): JSX.Element {
+    return (
+        <>
+            <h2>Not Found</h2>
+            <p>Could not find requested resource</p>
+        </>
+    );
+}
