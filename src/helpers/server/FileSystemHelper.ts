c3bc62106756897c05a6883d0a4cff4962fc3d08
Alan Morel <alan@alanmorel.com>
2023-04-25 14:06:51 -0400

    refactor: rename fileExists to pathExists

--- src/helpers/server/FileSystemHelper.ts
+++ src/helpers/server/FileSystemHelper.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 
-export const fileExists = async (path: string): Promise<boolean> => {
+export const pathExists = async (path: string): Promise<boolean> => {
     try {
         await fs.stat(path);
     } catch (error) {
