211042d722d287998c3ad1a72550b041c2131ea3
Alan Morel <alan@alanmorel.com>
2023-04-25 22:58:11 -0400

    feat: add isDateEarlier helper function

--- src/helpers/server/DateHelper.ts
+++ src/helpers/server/DateHelper.ts
@@ -8,3 +8,24 @@ export function getReadableDate(date: Date): string {
 export function addDays(date: Date, days: number): Date {
     return new Date(date.getTime() + 86400000 * days);
 }
+
+export function isDateEarlier(dateA: Date, dateB: Date): boolean {
+    const yearA = dateA.getFullYear();
+    const yearB = dateB.getFullYear();
+
+    if (yearA < yearB) {
+        return true;
+    }
+
+    const monthA = dateA.getMonth();
+    const monthB = dateB.getMonth();
+
+    if (yearA === yearB && monthA < monthB) {
+        return true;
+    }
+
+    const dayA = dateA.getDate();
+    const dayB = dateB.getDate();
+
+    return yearA === yearB && monthA === monthB && dayA < dayB;
+}
