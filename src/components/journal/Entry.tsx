13a485234a145b517f1f4325a67a29c29bc24a7d
Alan Morel <alan@alanmorel.com>
2023-05-02 15:08:32 -0400

    refactor: remove toast when entry is fetched

--- src/components/journal/Entry.tsx
+++ src/components/journal/Entry.tsx
@@ -63,8 +63,6 @@ export default function Entry(props: Props): JSX.Element {
             return;
         }
 
-        showInfoToast(`Loaded ${getReadableDate(date)}`);
-
         setEntry(response.data);
         setDate(date);
     };
