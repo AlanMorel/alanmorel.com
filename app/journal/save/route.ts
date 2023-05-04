a84ba051749e84a188ade039b1a792a13762942e
Alan Morel <alan@alanmorel.com>
2023-05-04 00:09:36 -0400

    feat: log on save for fallback

--- app/journal/save/route.ts
+++ app/journal/save/route.ts
@@ -1,4 +1,5 @@
 import { isJournalAuthenticated, saveJournalEntry } from "@/src/helpers/server/JournalHelper";
+import { Logger } from "@/src/helpers/server/Logger";
 import { sendFailure, sendSuccess } from "@/src/helpers/server/ResultHelper";
 import { NextResponse } from "next/server";
 
@@ -16,6 +17,8 @@ export async function POST(request: Request): Promise<NextResponse> {
     if (!result) {
         return sendFailure("failed to save entry");
     }
+
+    Logger.log(`Saved journal entry for ${date}: ${entry}`);
 
     return sendSuccess();
 }
