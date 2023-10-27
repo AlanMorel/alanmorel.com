e89f4025890fc9c1b0ad41226370f0f0218951ae
Alan Morel <alan@alanmorel.com>
2023-10-27 14:36:18 -0400

    fix: circular referencing

--- src/helpers/server/FileSystemHelper.ts
+++ src/helpers/server/FileSystemHelper.ts
@@ -1,34 +1,32 @@
-import logger from "@/src/helpers/server/Logger";
 import fs, { promises as fsAsync } from "fs";
 
-export const fileExists = async (path: string): Promise<boolean> => {
+export async function fileExists(path: string): Promise<boolean> {
     try {
         await fsAsync.stat(path);
     } catch (error) {
-        logger.critical(`Failed to check if file exists: ${JSON.stringify(error)}`);
         return false;
     }
 
     return true;
-};
+}
 
-export const createIfNotExists = (directory: string): void => {
+export function createIfNotExists(directory: string): void {
     if (!fs.existsSync(directory)) {
         fs.mkdirSync(directory, { recursive: true });
     }
-};
+}
 
-export const checkFileSize = (filePath: string): number => {
+export function checkFileSize(filePath: string): number {
     try {
         const stats = fs.statSync(filePath);
         return stats.size;
     } catch (error) {
         return -1;
     }
-};
+}
 
-export const deleteFile = (filePath: string): void => {
+export function deleteFile(filePath: string): void {
     try {
         fs.unlinkSync(filePath);
     } catch (error) {}
-};
+}
