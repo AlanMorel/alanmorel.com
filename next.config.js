65b442734a432479db266fde2c0b27f95f2a91b7
Alan Morel <alan@alanmorel.com>
2023-12-11 00:23:08 -0500

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 import fs from "fs";
 
+const safeStylesheet = [].join(" ");
+
+const safeScripts = ["*.googletagmanager.com"].join(" ");
+
+const contentSecurityPolicy = `style-src 'self' 'unsafe-eval' 'unsafe-inline' ${safeStylesheet}; script-src 'self' ${safeScripts} 'unsafe-eval' 'unsafe-inline';`;
+
 export function redirects() {
     try {
         const redirectsFile = fs.readFileSync("./src/redirects.json", "utf8");
@@ -17,6 +23,35 @@ const config = {
     },
     async redirects() {
         return redirects();
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: contentSecurityPolicy
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "camera=(), battery=(self), geolocation=(), microphone=()"
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "origin-when-cross-origin"
+                    }
+                ]
+            }
+        ];
     }
 };
 
