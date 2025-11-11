8bd3834186af47a3a00284bece9456be8c4bf7de
Alan Morel <alan@alanmorel.com>
2025-11-11 16:47:47 -0500

    feat: use sitemap for sitemap.xml

--- app/sitemap[.]xml.ts
+++ app/sitemap[.]xml.ts
@@ -1,30 +1,33 @@
 import Config from "@/src/Config.ts";
 import { createFileRoute } from "@tanstack/react-router";
+import { SitemapStream, streamToPromise } from "sitemap";
 
 const routes = ["", "/resume"];
 
-function getSitemapXML(routes: string[]): string {
-    const lastModified = new Date().toISOString();
+async function getSitemapXML(routes: string[]): Promise<string> {
+    const smStream = new SitemapStream({ hostname: Config.app.url });
 
-    return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${routes
-    .map(
-        route => `  <url>
-    <loc>${Config.app.url}${route}</loc>
-    <lastmod>${lastModified}</lastmod>
-  </url>`
-    )
-    .join("\n")}
-</urlset>`;
+    routes.forEach(route => {
+        const lastModified = new Date().toISOString();
+
+        smStream.write({
+            url: route,
+            lastmod: lastModified
+        });
+    });
+
+    smStream.end();
+
+    const sitemap = await streamToPromise(smStream);
+
+    return sitemap.toString();
 }
 
 export const Route = createFileRoute("/sitemap.xml")({
     server: {
         handlers: {
-            GET: () => {
-                const sitemap = getSitemapXML(routes);
-
+            GET: async () => {
+                const sitemap = await getSitemapXML(routes);
                 return new Response(sitemap, {
                     headers: {
                         "Content-Type": "application/xml"
