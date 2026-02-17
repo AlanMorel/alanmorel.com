9bf7990b1dd35f3b04143b060960b552928fd84e
Alan Morel <alan@alanmorel.com>
2026-02-17 02:17:40 -0500

    refactor: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,35 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+import { resolve } from "node:path";
+import type { AliasOptions } from "vite";
 import { defineConfig } from "vite";
 
+const alias: AliasOptions = {
+    "@": resolve(__dirname, ".")
+};
+
 export default defineConfig({
-    server: {
-        port: 8080
-    },
     plugins: [
         tanstackStart(),
         nitro({
-            preset: "node-server"
+            preset: "bun",
+            alias,
+            minify: true,
+            sourcemap: false,
+            compressPublicAssets: {
+                gzip: true,
+                brotli: true
+            }
         }),
-        viteReact(),
+        react(),
         tailwindcss()
-    ]
+    ],
+    server: {
+        port: 8080
+    },
+    resolve: {
+        alias
+    }
 });
