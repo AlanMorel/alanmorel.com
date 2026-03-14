0700e227efeceb82e05b29cebb37932bd863075b
Alan Morel <alan@alanmorel.com>
2026-03-14 03:46:40 -0400

    refactor: ensure pill carousel looks good on mobile

--- src/components/PillCarousel.tsx
+++ src/components/PillCarousel.tsx
@@ -17,15 +17,11 @@ export default function PillCarousel(props: Readonly<Prop>): ReactElement {
     const items = reverse ? [...pills].reverse() : pills;
 
     return (
-        <div className="mx-auto max-w-240 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="mx-auto w-full max-w-240 overflow-hidden mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
             <ul className={reverse ? "animate-marquee-reverse flex w-max" : "animate-marquee flex w-max"}>
                 {[...items, ...items].map((pill, i) => (
                     <li className="inline-flex shrink-0" key={`${pill.label}-${i}`}>
-                        <Pill
-                            className="bg-slate-200 dark:bg-slate-200"
-                            Icon={pill.icon}
-                            label={pill.label}
-                        />
+                        <Pill className="bg-slate-200 dark:bg-slate-200" Icon={pill.icon} label={pill.label} />
                     </li>
                 ))}
             </ul>
